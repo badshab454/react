27d6f096ec72bee64aace4a902b4b8d2ccfee87d
Badsha Hossain <badshab454@gmail.com>
2023-08-20 11:35:45 +0600

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-        {count}
-        <button onClick={() => setCount(count+10)}>Increment button</button>
+      {count}
+      <button onClick={() => setCount(count + 10)}>Increment use button</button>
     </>
-  )
-}
+  );
+};
 
 export default App;
