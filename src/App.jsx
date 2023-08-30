8a68caf9822321f11a88f4c5ebb29c185e67bee9
Badsha Hossain <badshab454@gmail.com>
2023-08-30 13:29:49 +0600

    react dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import MyButton from "./components/MyButton";
 
 const App = () => {
-  return <MyButton />;
+  return (
+    <>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+    </>
+  );
 };
 
 export default App;
