1b998ea8c953263d8858a709a9b24d521b7dd9e8
Badsha Hossain <badshab454@gmail.com>
2023-08-30 17:11:26 +0600

    chai aur value

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 
 
 const App = () => {
-  return <h1>Chai aur React.</h1>;
+  return <>
+  <h1>Chai aur react</h1>
+  <h2>Counter value: 5</h2>
+
+  <button>Add Value</button>
+  <br />
+  <br />
+  <button>Remove Value</button>
+  </>;
 };
 
 export default App;
